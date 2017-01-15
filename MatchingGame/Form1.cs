739aded82bdc4001daee769f63d31e701b8af9e1
Matt <mattaecole@gmail.com>
2017-01-15 13:27:01 -0500

    Finished part 2. Added random variable and list.

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,9 +12,25 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon in the Webdings font, and each icon appears twice in this list
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
