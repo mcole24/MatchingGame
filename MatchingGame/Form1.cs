eb389e57771533f32543ca3fdbd21faa061e23b9
Matt <mattaecole@gmail.com>
2017-01-15 14:02:41 -0500

    Finished part 4

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -26,11 +26,53 @@ namespace MatchingGame
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary> 
+        /// Assign each icon from the list of icons to a random square 
+        /// </summary> 
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels, 
+            // and the icon list has 16 icons, 
+            // so an icon is pulled at random from the list 
+            // and added to each label 
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        /// <summary> 
+        /// Every label's Click event is handled by this event handler 
+        /// </summary> 
+        /// <param name="sender">The label that was clicked</param>
+        /// <param name="e"></param>
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // If the clicked label is black, the player clicked an icon that's already been revealed -- ignore the click 
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                clickedLabel.ForeColor = Color.Black;
+            }
         }
     }
 }
